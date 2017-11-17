0ab0f07ed5de9dc3530ea156e5dbcb6983c9f04f
Kate Sohng <kheeya7@hotmail.com>
2017-11-16 22:33:49 -0800

    Adding Bootstrap Navigation

--- src/header/view.js
+++ src/header/view.js
@@ -1,9 +1,17 @@
 import Backbone from 'backbone';
+import $ from 'jquery';
 import template from './template.pug';
 
 export class Header extends Backbone.View {
     render() {
         this.$el.html(template());
+
+        this.$('nav').affix({
+            offset:{  
+                // TODO: we need to fix this so that it uses the top-section height              
+                top : 280 - this.$('nav').height()
+            }
+        })
 
         return this;
     }
