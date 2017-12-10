14a90c6dd9d29a2b614df48984ddd307b501456e
Younghoon Gim <khunny7@live.com>
2017-12-10 15:12:54 -0800

    content layout changes, image additions

--- src/sections/solution/view.js
+++ src/sections/solution/view.js
@@ -1,9 +1,12 @@
 import Backbone from 'backbone';
 import template from './template.pug';
+import paperProtoPic from './paper-proto.jpg';
 
 export class Solution extends Backbone.View {
     render() {
-        this.$el.html(template());
+        this.$el.html(template({
+            paperProtoPic,
+        }));
 
         return this;
     }
